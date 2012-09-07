3d2ba05a3e9120465d532ae82a7c5546579fadb0
Jeremy Mills <jeremy.mills89@gmail.com>
2012-09-07 10:53:08 -0400

    first examples of php

---
+++ weeks/1/hello-world.php
@@ -0,0 +1,9 @@
+<?php
+//two primary methods of printing to the screen :echo: or :print:
+echo 'Hello World';
+//single quotations treated as literal strings double as processed
+print '<br /> <br />'; //line breaks in html
+
+print 'Bye World';
+
+?>
---
+++ weeks/1/variables.php
@@ -0,0 +1,24 @@
+<?php
+//variables characters always begin with $ and followed by anything but numbers
+define('LNBREAK_HTML','<br /><br />');
+
+$hello_world = 'Hello World';
+
+echo $hello_world;
+echo LNBREAK_HTML;
+
+$hello = 'Hello';
+$world = 'World';
+
+echo $hello .' '. $world;
+echo LNBREAK_HTML;
+
+print "{$hello} {$world}";
+echo LNBREAK_HTML;
+
+for( $i = 0; $i < 10; ++$i )
+{
+	print $1 . LNBREAK_HTML;
+}
+
+?>
