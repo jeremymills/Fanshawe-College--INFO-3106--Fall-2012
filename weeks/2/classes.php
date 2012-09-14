3508a0fc962d2467d7243a2b824c250ed3cab499
Jeremy Mills <jeremy.mills89@gmail.com>
2012-09-14 09:59:32 -0400

    Beginning to defining classes

---
+++ weeks/2/classes.php
@@ -0,0 +1,114 @@
+<?php
+//ALWAYS include next two lines!!
+ini_set('display_errors', 'on');
+error_reporting(E_ALL | E_STRICT);
+
+function myFuncInterface(IMyInterface $o) {
+	$o->myPreDefinedMethod();
+}
+function myFuncBaseClass(MyBaseClass $o) {
+	$o->myMethod();
+	//$o->myChildMethod(); //is not accessible
+}
+function myFuncChildClass(MyChildClass $o) {
+	$o->myChildMethod();// accessible here...
+	$o->myMethod();// all parent class's methods are accissible aswell...
+}
+
+interface IMyInterface {
+	public function myPreDefinedMethod();
+}
+
+class MyBaseClass implements IMyInterface {
+	
+	const MY_CONST = '1';
+
+	private $_my_private_var;
+	protected $_my_protected_var;
+	public $_my_public_var;
+	
+	/**
+	 * Constructor Method
+	 */
+	public function __construct() {
+		print __METHOD__ . '<br />';
+		
+		$this->_my_private_var = 'private';
+		$this->_my_protected_var = 'protected';
+		$this->_my_public_var = 'public';
+		
+		$this->myPrivateMethod();
+	}
+	/**
+	 * Destructor Method
+	 */
+	public function __destruct() {
+		print __METHOD__ . '<br />';
+	}
+	/**
+	 * Sample Method
+	 */	
+	public function myMethod() {
+		print __METHOD__ . '<br />';
+	}
+	/**
+	 * Protected Method
+	 */	
+	protected function myProtectedMethod() {
+		print __METHOD__ . '<br />';
+	}
+	/**
+	 * Private Method
+	 */	
+	private function myPrivateMethod() {
+		print __METHOD__ . '<br />';
+	}
+	
+	public function myPreDefinedMethod() {
+		print __METHOD__. '<br />';
+	}
+}
+
+class MyChildClass extends MyBaseClass {
+	public function __construct() {
+		print __METHOD__ . '<br />';
+		
+		parent::__construct();
+	}
+	public function myChildMethod() {
+		print __METHOD__ . '<br />';
+	}
+	public static function myStaticMethod() {
+		print __METHOD__ . '<br />';
+	}
+}
+
+/* Base Class Specific */
+$base_class = new MyBaseClass();
+$base_class->myMethod();
+/*
+$base_class->myProtectedMethod();// won't run ... not visible
+$base_class->myPrivateMethod(); // won't run ... not visible
+*/
+print '<br />';
+//print_r($base_class);
+
+//unset($base_class);
+print '<br /><br />';
+
+$child = new MyChildClass();
+MyChildClass::myStaticMethod();
+
+print '<br /><br />';
+print '<br /><br />';
+
+myFuncInterface($base_class);
+myFuncInterface($child); //still works, why?
+
+myFuncBaseClass($base_class); //base class instance
+myFuncBaseClass($child);//still works, why?
+
+myFuncChildClass($child);
+myFuncChildClass($base_class);// doesn't work, why?
+
+
