893d3068547f4ad66df7904fca5927b25514bdba
Jeremy Mills <jeremy.mills89@gmail.com>
2012-09-14 08:31:59 -0400

    Created new functions.php for examples

---
+++ weeks/2/functions.php
@@ -0,0 +1,25 @@
+<?php
+//ALWAYS include next two lines!!
+ini_set('display_errors', 'on');
+error_reporting(E_ALL | E_STRICT);
+
+/**
+ * print_ln
+ * 
+ * Prints the desired string.
+ *
+ * @access public
+ * @param string Contains the string to print
+ * @return void
+ */
+ function print_ln($line) {
+	print $line . '<br />';
+ }
+
+print_ln('Hello World');
+
+
+
+
+
+?>
