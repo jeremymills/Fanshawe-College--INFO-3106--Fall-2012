05e30d9b4928e0e07672e62d15c4ea8994007fb5
Jeremy Mills <jeremy.mills89@gmail.com>
2012-09-14 10:25:10 -0400

    improvments on include / include_once ... require / require_once

--- weeks/2/classes.php
+++ weeks/2/classes.php
@@ -3,85 +3,11 @@
 ini_set('display_errors', 'on');
 error_reporting(E_ALL | E_STRICT);
 
-function myFuncInterface(IMyInterface $o) {
-	$o->myPreDefinedMethod();
-}
-function myFuncBaseClass(MyBaseClass $o) {
-	$o->myMethod();
-	//$o->myChildMethod(); //is not accessible
-}
-function myFuncChildClass(MyChildClass $o) {
-	$o->myChildMethod();// accessible here...
-	$o->myMethod();// all parent class's methods are accissible aswell...
-}
+require_once './classes_include.php';// will stop at first sign of file error
 
-interface IMyInterface {
-	public function myPreDefinedMethod();
-}
+//include_once './classes_include.php';// will keep trying everthing if file error
 
-class MyBaseClass implements IMyInterface {
-	
-	const MY_CONST = '1';
-
-	private $_my_private_var;
-	protected $_my_protected_var;
-	public $_my_public_var;
-	
-	/**
-	 * Constructor Method
-	 */
-	public function __construct() {
-		print __METHOD__ . '<br />';
-		
-		$this->_my_private_var = 'private';
-		$this->_my_protected_var = 'protected';
-		$this->_my_public_var = 'public';
-		
-		$this->myPrivateMethod();
-	}
-	/**
-	 * Destructor Method
-	 */
-	public function __destruct() {
-		print __METHOD__ . '<br />';
-	}
-	/**
-	 * Sample Method
-	 */	
-	public function myMethod() {
-		print __METHOD__ . '<br />';
-	}
-	/**
-	 * Protected Method
-	 */	
-	protected function myProtectedMethod() {
-		print __METHOD__ . '<br />';
-	}
-	/**
-	 * Private Method
-	 */	
-	private function myPrivateMethod() {
-		print __METHOD__ . '<br />';
-	}
-	
-	public function myPreDefinedMethod() {
-		print __METHOD__. '<br />';
-	}
-}
-
-class MyChildClass extends MyBaseClass {
-	public function __construct() {
-		print __METHOD__ . '<br />';
-		
-		parent::__construct();
-	}
-	public function myChildMethod() {
-		print __METHOD__ . '<br />';
-	}
-	public static function myStaticMethod() {
-		print __METHOD__ . '<br />';
-	}
-}
+//include_once './classes_include.php'; //file only gets included once
 
 /* Base Class Specific */
 $base_class = new MyBaseClass();
