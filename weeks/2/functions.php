02c23b196b5ba2ec93c1fc01374ad109c095ac7c
Jeremy Mills <jeremy.mills89@gmail.com>
2012-09-14 08:42:59 -0400

    Added ln_break in print_ln() to have a default val of true, added my_trim

--- weeks/2/functions.php
+++ weeks/2/functions.php
@@ -13,15 +13,31 @@ error_reporting(E_ALL | E_STRICT);
  * @param bool Deteremines whether a new line is to be used.
  * @return void
  */
- function print_ln($line, $ln_break = true) {
-	print $line;
+function print_ln($line, $ln_break = true) {
+	print my_trim($line);
 	if( $ln_break ) {
 		print '<br />';
 	}
- }
+}
+ 
+/**
+ * my_trim
+ *
+ * Trims the specified string of whitespace using the trim function.
+ *
+ * @access public
+ * @param string Contains the string to be trimmed.
+ * @return string Returns the trimmed string
+ */
 
+function my_trim($str) {
+	return trim($str);
+}
+ 
+
+print_ln('First and Formost');
 print_ln('Hello World');
-
+print_ln(' Hello world from Foo Bar ... ');
 
 
 
