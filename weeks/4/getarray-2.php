d70ac5486b55b97b0e796fb6fe3ce835f0ce0767
Jeremy Mills <jeremy.mills89@gmail.com>
2012-09-26 19:43:45 -0400

    building upon forms with getarrays

---
+++ weeks/4/getarray-2.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>GET Array with Forms</title>
+</head>
+<body>
+
+<div style="padding: 10px 10px; background #eee;">
+	<?php print_r($_GET); ?>
+</div>
+
+
+<form method="get" action="getarray-2.php">
+	<p><label for="search">Search:</label> <input type="text" name="search" id="search" value="" maxlength="45" /></p>
+
+<p><input 
+
+</form>
+
+</body>
+</html>
